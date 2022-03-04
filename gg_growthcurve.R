fba3b30c59224eb98e61c1f85d96c562cd0eb860
André Barros <andrebolerbarros@gmail.com>
2022-03-04 06:28:59 +0000

    Update gg_growthcurve.R
    Removed the run of "SummarizeGrowth" run inside, in order for users to run it outside of the function and, thus, save its output.

--- gg_growthcurve.R
+++ gg_growthcurve.R
@@ -1,18 +1,13 @@
-gg_growthcurve <- function (Time=NA,Well=NA, background="min", 
-                            blank=NA, transp=0.7,linetype="solid",
+gg_growthcurve <- function (Curve=NA, transp=0.7,linetype="solid",
                             col_points="black",col_line="red",pt_size=1,
                             line_size=1){
   
   require(ggplot2)
   require(growthcurver)
   
-  if(!length(Time)==length(Well)) stop("Time and Well should have same length!")
-  if(!is.numeric(Time)) stop("Time must be numeric!")
-  if(!is.numeric(Well)) stop("Well must be numeric!")
-  
-  m0<- SummarizeGrowth(Time, Well,bg_correct = background)
-  
-  ss1<-data.frame(Time=m0$data$t,OD=m0$data$N,Exp=predict(m0$model))
+  if(!class(Curve)=="gcfit") stop("This is not a SummarizeGrowthCurve object!")
+ 
+  ss1<-data.frame(Time=Curve$data$t,OD=Curve$data$N,Exp=predict(Curve$model))
 
   ggplot(ss1,aes(x=Time,y=OD))+
     geom_point(alpha=transp,colour=col_points,size=pt_size)+
